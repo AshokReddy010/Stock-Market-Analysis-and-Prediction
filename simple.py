5c740afff8643af129acb585076f211f5d670b83
Gopinadh110 <gpragada@albany.edu>
2025-02-25 17:27:03 -0500

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,2 +1,3 @@
 print("Hello Team Currently we are working on Stock Market Analysis and future prediction project")
 print(" Hello team this is Divya Sri Jampana")
+print(" Hello team this is gopinadh")
